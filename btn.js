d54396005f512cc5c5915ee3f48e32adc1daa606
OppasVTzin <144271865+OppasVTzin@users.noreply.github.com>
2024-05-11 20:04:18 -0300

    Commit 1

---
+++ btn.js
@@ -0,0 +1,29 @@
+var home = document.getElementById("btn1");
+home.innerText = "Home"
+home.style.width = "3cm"
+home.style.position = "Absolute"
+home.style.top = "30px"
+home.style.height = "2cm"
+home.style.right = "500px"
+home.style.color = "white"
+home.style.backgroundColor = "Red"
+
+var bio = document.getElementById("btn2");
+bio.innerText = "Biography"
+bio.style.width = "3cm"
+bio.style.position = "Absolute"
+bio.style.top = "30px"
+bio.style.height = "2cm"
+bio.style.right = "385px"
+bio.style.color = "white"
+bio.style.backgroundColor = "Red"
+
+var School = document.getElementById("btn3");
+School.innerText = "School"
+School.style.width = "3cm"
+School.style.position = "Absolute"
+School.style.top = "30px"
+School.style.height = "2cm"
+School.style.right = "270px"
+School.style.color = "white"
+School.style.backgroundColor = "Red"
